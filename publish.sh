71bb55c8d72413123d426bc43953913f851fcd39
LaczkóBalázs <lab@frankly.dk>
2024-01-07 16:45:18 +0100

    updated the build script

--- publish.sh
+++ publish.sh
@@ -6,10 +6,6 @@ version=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" NetCurses.csproj)
 version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 sed -i "s/<VersionPrefix>.*<\/VersionPrefix>/<VersionPrefix>$version<\/VersionPrefix>/" NetCurses.csproj
 
-git add NetCurses.csproj
-git commit -m "Bump version to $version"
-git push
-
 # build and publish
 dotnet pack -c Release -o ./nuget
-dotnet nuget push ./nuget/NetCurses.$version.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./nuget/Blaczko.NetCurses.$version.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
