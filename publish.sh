12b015461da4bd40761ace9c095c97bcdaf20cc1
LaczkóBalázs <lab@frankly.dk>
2024-01-07 16:48:35 +0100

    updating build script

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 sed -i "s/<VersionPrefix>.*<\/VersionPrefix>/<VersionPrefix>$version<\/VersionPrefix>/" NetCurses.csproj
 
 # build and publish
-dotnet pack -c Release -o ./nuget
+dotnet pack -c Release -o ./nuget -p:PackageID=Blaczko.NetCurses
 dotnet nuget push ./nuget/Blaczko.NetCurses.$version.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
