dd84f1579d9f73e6507a3cbaa9952dde2812fd12
LaczkóBalázs <lab@frankly.dk>
2024-01-07 16:21:07 +0100

    initial commit

---
+++ NetCurses/Models/UpdateResult.cs
@@ -0,0 +1,7 @@
+﻿namespace NetCurses.Models;
+public class UpdateResult
+{
+    public string WindowId { get; set; } = string.Empty;
+    public bool RefreshWindow { get; set; } = true;
+    public bool RemoveSelfFromParent { get; set; } = false;
+}
